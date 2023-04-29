74f456923b9d684ec89d324c9a3a4802714df071
kyzym <kyzym1@gmail.com>
2023-04-29 08:37:41 +0200

    feat: add keyboarRendering 04/29/2023 08:37:41

--- index.js
+++ index.js
@@ -1,196 +1,46 @@
-/* eslint-disable import/extensions */
-import keyLayout from './js/keyLayout.js';
+const iconLink = document.createElement('link');
+iconLink.rel = 'icon';
+iconLink.href = 'assets/keyboard-key.png';
+document.head.appendChild(iconLink);
 
-const container = document.createElement('div');
-container.classList.add('container');
+let currentLanguage = localStorage.getItem('lang')
+  ? localStorage.getItem('lang')
+  : 'ru_RU';
+localStorage.setItem('lang', currentLanguage);
 
-const keyboardContainer = document.createElement('div');
-keyboardContainer.classList.add('keyboard');
+const inputArea = document.createElement('textarea');
+inputArea.cols = 91;
+inputArea.rows = 10;
+inputArea.wrap = '\n';
+document.body.append(inputArea);
+setInterval(() => {
+  inputArea.focus();
+}, 0);
 
-const textArea = document.createElement('textarea');
-textArea.classList.add('text-input');
-document.body.insertBefore(textArea, document.getElementById('keyboard'));
+const virtualKeyboard = document.createElement('div');
+virtualKeyboard.className = 'keyboard';
+document.body.append(virtualKeyboard);
 
-let currentLanguage = localStorage.getItem('language') || 'en';
-let isShiftPressed = false;
-let isCapsLockPressed = false;
-let isAltPressed = false;
+async function fetchKeyboardData() {
+  const keyLayoutPath = 'assets/keyLayout.json';
+  const response = await fetch(keyLayoutPath);
+  const data = await response.json();
+  return data;
+}
 
-const preventDefaultForSpecialKeys = (event) => {
-  const specialKeys = [
-    'ShiftLeft',
-    'ShiftRight',
-    'ControlLeft',
-    'ControlRight',
-    'AltLeft',
-    'AltRight',
-    'CapsLock',
-    'Tab',
-    'ArrowUp',
-    'ArrowDown',
-    'ArrowLeft',
-    'ArrowRight',
-    'Delete',
-  ];
+function createKeyElement(key) {
+  const newKeyElement = document.createElement('div');
+  newKeyElement.className = key.category;
+  newKeyElement.id = key.code;
+  newKeyElement.innerHTML = key.value;
+  virtualKeyboard.append(newKeyElement);
+}
 
-  if (specialKeys.includes(event.code)) {
-    event.preventDefault();
-  }
-};
-
-const syncLanguageWithSystem = () => {
-  const systemLanguage = navigator.language.startsWith('ru') ? 'ru' : 'en';
-  if (systemLanguage !== currentLanguage) {
-    toggleLanguage();
-  }
-};
-
-const initListeners = () => {
-  document.getElementById('keyboard').addEventListener('click', (event) => {
-    if (event.target.classList.contains('key')) {
-      handleKeyPress(event, true);
-    }
+function renderKeyboard(language) {
+  fetchKeyboardData().then((result) => {
+    const keyboardData = result.languages[language];
+    keyboardData.forEach(createKeyElement);
   });
+}
 
-  document.addEventListener('keydown', (event) => {
-    preventDefaultForSpecialKeys(event);
-    if (event.code === 'AltLeft' || event.code === 'AltRight') {
-      isAltPressed = true;
-    }
-    if (isAltPressed && event.shiftKey) {
-      toggleLanguage();
-    }
-    if (keyHandlers[event.code]) {
-      keyHandlers[event.code]();
-    } else {
-      handleKeyPress(event);
-    }
-    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
-      toggleShift(true);
-    }
-  });
-
-  document.addEventListener('keyup', (event) => {
-    if (event.code === 'AltLeft' || event.code === 'AltRight') {
-      isAltPressed = false;
-    }
-    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
-      toggleShift(false);
-    }
-  });
-
-  document.addEventListener('DOMContentLoaded', syncLanguageWithSystem);
-};
-
-const updateKeyTextContent = (keyElement, keyObj) => {
-  const lang = currentLanguage + (isShiftPressed ? 'Shift' : '');
-  const char = keyObj[lang];
-  keyElement.textContent =
-    isCapsLockPressed && char.match(/[a-zа-я]/i) ? char.toUpperCase() : char;
-};
-
-const createKey = (keyObj) => {
-  const keyElement = document.createElement('button');
-  keyElement.setAttribute('type', 'button');
-  keyElement.classList.add('key');
-  keyElement.dataset.code = keyObj.code;
-  keyElement.dataset.key = keyObj[currentLanguage];
-  updateKeyTextContent(keyElement, keyObj);
-  return keyElement;
-};
-
-const updateKeys = () => {
-  keyLayout.flat().forEach((keyObj) => {
-    const keyElement = document.querySelector(
-      `.key[data-code="${keyObj.code}"]`
-    );
-    if (keyElement) {
-      updateKeyTextContent(keyElement, keyObj);
-    }
-  });
-};
-
-const toggleLanguage = () => {
-  currentLanguage = currentLanguage === 'en' ? 'ru' : 'en';
-  localStorage.setItem('language', currentLanguage);
-  updateKeys();
-};
-
-const toggleShift = (isPressed) => {
-  if (isShiftPressed !== isPressed) {
-    isShiftPressed = isPressed;
-    updateKeys();
-  }
-};
-
-const toggleCapsLock = () => {
-  isCapsLockPressed = !isCapsLockPressed;
-  updateKeys();
-};
-
-const handleKeyPress = (event, isVirtual = false) => {
-  const code =
-    event instanceof KeyboardEvent ? event.code : event.target.dataset.code;
-  const keyObj = keyLayout.flat().find((key) => key.code === code);
-
-  if (keyObj) {
-    const lang = currentLanguage + (isShiftPressed ? 'Shift' : '');
-    const char = keyObj[lang];
-
-    if (char.length === 1 || isVirtual) {
-      textArea.setRangeText(
-        char,
-        textArea.selectionStart,
-        textArea.selectionEnd,
-        'end'
-      );
-    }
-  }
-};
-
-const keyHandlers = {
-  CapsLock: toggleCapsLock,
-  Tab: () => {
-    textArea.setRangeText(
-      '\t',
-      textArea.selectionStart,
-      textArea.selectionEnd,
-      'end'
-    );
-  },
-  Delete: () => {
-    if (textArea.selectionStart !== textArea.selectionEnd) {
-      textArea.setRangeText(
-        '',
-        textArea.selectionStart,
-        textArea.selectionEnd,
-        'end'
-      );
-    } else if (textArea.selectionStart < textArea.value.length) {
-      textArea.setRangeText(
-        '',
-        textArea.selectionStart,
-        textArea.selectionStart + 1,
-        'end'
-      );
-    }
-  },
-};
-
-const initKeys = () => {
-  keyLayout.forEach((row) => {
-    const rowElement = document.createElement('div');
-    rowElement.classList.add('row');
-    row.forEach((keyObj) => {
-      const keyElement = createKey(keyObj);
-      rowElement.appendChild(keyElement);
-    });
-    keyboardContainer.appendChild(rowElement);
-  });
-
-  container.appendChild(keyboardContainer);
-  document.body.appendChild(container);
-};
-
-initKeys();
-initListeners();
+renderKeyboard(currentLanguage);
